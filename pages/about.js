8c0e64ab0d2f931e0e4566b2093ea709e154e418
Stevie McCullough <stephen.mccullough@gmail.com>
2023-02-24 02:43:17 +0000

    WIP

--- pages/about.js
+++ pages/about.js
@@ -8,24 +8,22 @@ export default function About() {
     <>
       <Layout about>
         <section className={styles.headingMd}>
-          <p>
-            {" "}
+        <p class="font-light text-gray-500 dark:text-gray-400">
             This is my little corner of the internet where I can tinker on
             things. I'm currently working on a few projects, one of which is
-            this website.{" "}
-          </p>{" "}
-          <p>
-            {" "}
+            this website.
+          </p>
+        <p class="mt-2 font-light text-gray-500 dark:text-gray-400">
             I'm building this site using{" "}
             <a href="https://nextjs.org/">Next.js</a>, which is a{" "}
             <a href="https://reactjs.org/">React</a> framework.{" "}
-          </p>{" "}
-          <p>
+          </p>
+        <p class="mt-2 font-light text-gray-500 dark:text-gray-400">
             {" "}
             I'm learning as I go, so if you see anything that could be improved,
             please let me know (via a{" "}
             <a href="https://github.com/swmcc/swm.cc/pulls">PR</a> or an{" "}
-            <a href="https://github.com/swmcc/swm.cc/issues">Issue</a>).{" "}
+            <a href="https://github.com/swmcc/swm.cc/issues">Issue</a>).
           </p>
         </section>
       </Layout>
--- pages/index.js
+++ pages/index.js
@@ -8,11 +8,20 @@ export default function Home() {
   return (
     <Layout home>
       <section className={styles.headingMd}>
-        <p>
-          Hi, I'm <strong>Stephen</strong>. I'm a software engineer, currently
-          working for <em>$BIG_CORP</em>. I'm also but not limited to being a
-          self confessed coffee snob, aspiring gardener, ever frustrated golfer
-          and all round idiot.
+        <p class="font-light text-gray-500 dark:text-gray-400">
+          Hi, I'm{" "}
+          <strong class="font-semibold text-gray-900 dark:text-white">
+            Stephen
+          </strong>
+          . More commonly known as SWM (swim). I'm a software engineer,
+          currently working for <em class="font-italic">$BIG_CORP</em>. I'm also
+          but not limited to being a self confessed coffee snob, aspiring
+          gardener, ever frustrated golfer and all round idiot.
+        </p>
+        <p class="mt-3 font-light text-gray-500 dark:text-gray-400">
+          If you want to find out more about me, click&nbsp;
+          <Link href="/about">here</Link>. Or if you want to contact me then
+          send one of those fancy new electronic mail thingymajigs to me here.
         </p>
       </section>
     </Layout>
